ff8063e4b44d860753016ae198e8a0309f6681c5
Soobs <soobin@hsua.com.au>
2020-08-07 12:48:26 +1000

    Convert class to hooks

--- WordRelay.jsx
+++ WordRelay.jsx
@@ -1,56 +1,43 @@
 const React = require('react');
-const { Component } = React;
+const { useState, useRef } = React;
 
-class WordRelay extends Component {
-  state = {
-    word: '감자',
-    value: '',
-    result: '',
-  };
+const WordRelay = () => {
+  const [word, setWord] = useState('감자');
+  const [value, setValue] = useState('');
+  const [result, setResult] = useState('');
+  const inputRef = useRef(null);
 
-  onSubmitForm = (e) => {
+  const onSubmitForm = (e) => {
     e.preventDefault();
 
-    if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
-      this.setState({
-        result: '딩동댕',
-        word: this.state.value,
-        value: ''
-      });
+    if (word[word.length - 1] === value[0]) {
+      setResult('딩동댕');
+      setWord(value);
+      setValue('');
 
-      this.input.focus();
+      inputRef.current.focus();
     } else {
-      this.setState({
-        result: '땡',
-        value: ''
-      });
+      setResult('땡');
+      setValue('');
 
-      this.input.focus();
+      inputRef.current.focus();
     }
   };
 
-  onChangeInput = (e) => {
-    this.setState({ value: e.target.value })
+  const onChangeInput = (e) => {
+    setValue({ value: e.target.value });
   };
 
-  input; // Why?
-
-  onRefInput = (c) => {
-    this.input = c;
-  };
-
-  render() {
-    return (
+  return (
       <>
-        <div>{this.state.word}</div>
-        <form onSubmit={this.onSubmitForm}>
-          <input ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput} />
+        <div>{word}</div>
+        <form onSubmit={onSubmitForm}>
+          <input ref={inputRef} value={value} onChange={onChangeInput} />
           <button>입력!</button>
         </form>
-        <div>{this.state.result}</div>
+        <div>{result}</div>
       </>
-    );
-  }
+  );
 }
 
 module.exports = WordRelay;
